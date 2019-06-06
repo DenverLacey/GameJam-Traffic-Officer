ed59b40387d3dcfcf9829fe5b5591207cfb1c340
Tory-Kruger <39174598+Tory-Kruger@users.noreply.github.com>
2019-06-06 10:20:07 +1000

    Unity project added

---
+++ OcGO Test/Assets/Scripts/VRRaycaster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRaycaster : MonoBehaviour
+{
+    public bool validHit;
+    public RaycastHit lastHitObject;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out lastHitObject))
+        {
+            validHit = true;
+        }
+        else
+        {
+            validHit = false;
+        }
+    }
+
+    public bool GetIsHit()
+    {
+        return validHit;
+    }
+
+    public Vector3 GetHitPosition()
+    {
+        if (validHit)
+            return lastHitObject.point;
+        return Vector3.zero;
+    }
+
+    public GameObject GetHitObject()
+    {
+        if (validHit)
+            return lastHitObject.collider.gameObject;
+
+        return null;
+    }
+
+}
---
+++ OcGO Test/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    public bool activated;
+
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activated)
+        {
+            GetComponent<Rigidbody>().AddTorque(Vector3.right * rotationSpeed, ForceMode.Impulse);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        activated = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
