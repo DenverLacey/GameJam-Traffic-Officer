4e3f093130e171477c81eae48714081dc3c007df
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2019-06-06 10:48:33 +1000

    - Unversioned .vs folder - Added PlayerActor script

---
+++ OcGO Test/Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+
+public class PlayerActor : MonoBehaviour {
+
+	[Tooltip("Button to tell a car to stop")]
+	[SerializeField] private OVRInput.Button m_stopButton = OVRInput.Button.PrimaryIndexTrigger;
+
+	[Tooltip("Button to tell a car to go")]
+	[SerializeField] private OVRInput.Button m_goButton = OVRInput.Button.PrimaryTouchpad;
+
+	[Tooltip("MAximum distance of pointer raycast")]
+	[SerializeField] private float m_pointerDistance = 100f;
+
+	public int Score { get; set; }
+
+	// Update is called once per frame
+	void Update() {
+		if (OVRInput.GetDown(m_stopButton))
+			StopCar();
+		else if (OVRInput.GetDown(m_goButton))
+			GoCar();
+	}
+
+	void StopCar() {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, m_pointerDistance)) {
+			//Car = maybeCar = hit.collider.GetComponent<Car>();
+			//if (maybeCar != null) {
+			//	maybeCar.Stop();
+			//}
+		}
+	}
+
+	void GoCar() {
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, m_pointerDistance)) {
+			//Car = maybeCar = hit.collider.GetComponent<Car>();
+			//if (maybeCar != null) {
+			//	maybeCar.Go();
+			//}
+		}
+	}
+}
